a90d6e8c3911f30e9987735ba07411ba5c2fb870
Ariadust <980868243@qq.com>
2025-07-19 19:13:30 +0800

    修正插件名称和主页链接以保持一致性

--- plugins/nonebot_plugin_AniPush/__init__.py
+++ plugins/nonebot_plugin_AniPush/__init__.py
@@ -2,11 +2,11 @@
 from nonebot.plugin import PluginMetadata
 
 __plugin_meta__ = PluginMetadata(
-    name="AniPusher",
-    description="AniPusher：一个动画更新推送插件，支持Emby及AniRss的Webhook信息推送",
-    usage="AniPusher：一个动画更新推送插件，支持Emby及AniRss的Webhook信息推送",
+    name="nonebot-plugin-anipusher",
+    description="nonebot-plugin-anipusher：一个动画更新推送插件，支持Emby及AniRss的Webhook信息推送",
+    usage="nonebot-plugin-anipusher：一个动画更新推送插件，支持Emby及AniRss的Webhook信息推送",
     type="application",
-    homepage="https://github.com/AriadusTT/nonebot-plugin-AniPush",
+    homepage="https://github.com/AriadusTT/nonebot-plugin-anipusher",
     supported_adapters={"~onebot.v11"}
 )
 
