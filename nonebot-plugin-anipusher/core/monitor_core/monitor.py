b9ba5e4816aff85b694190bacf6ebc391be8b599
Ariadust <980868243@qq.com>
2025-08-02 00:25:42 +0800

    重写监控方法，使用nonebot兼容层添加路由

--- nonebot-plugin-anipusher/core/monitor_core/monitor.py
+++ nonebot-plugin-anipusher/core/monitor_core/monitor.py
@@ -1,39 +1,37 @@
 
-from fastapi import Request
+from nonebot import get_driver
+from nonebot.drivers import URL, Request, Response, ASGIMixin, HTTPServerSetup
 import asyncio
-from fastapi.responses import JSONResponse, PlainTextResponse
-from nonebot import get_driver, get_app
-from .processing_engine import DataProcessor
 from nonebot import logger
+from .processing_engine import DataProcessor
 
 
 class Monitor:
     def __init__(self):
         self.driver = get_driver()
-        self.app = get_app()
         self.host = self.driver.config.host
         self.port = self.driver.config.port
 
     async def start_monitor(self):
         """
-        启动监控服务
+        启动新的监控服务
         """
-        # 这里可以添加更多的监控逻辑
-        @self.app.post("/webhook")
-        async def monitor(request: Request):
-            data = await request.json()
-            logger.opt(colors=True).info(f"<lg>收到监控请求</lg>\n{data}")
+        async def handle_webhook(request: Request) -> Response:
+            data = request.json
+            logger.opt(colors=True).info(f"<lg>收到新的监控请求</lg>\n{data}")
             asyncio.create_task(DataProcessor.create_and_run(data))
-            return JSONResponse(status_code=200, content={"message": "ok"})
-        logger.opt(colors=True).success(
-            f"🔍 监控服务已启动，监听地址: <cyan>{self.host}:{self.port}/webhook</cyan>")
+            return Response(200,
+                            headers={"Content-Type": "application/json"},
+                            content="ok")
 
-        @self.app.get("/webhook")
-        async def monitor_page(request: Request):
-            """
-            监控页面
-            """
-            return PlainTextResponse(
-                "这是一个监控 Webhook 端点，请使用 POST 方法发送 JSON 数据到此端点。\n"
-                f"监控服务运行中，地址: {self.host}:{self.port}/webhook"
+        if isinstance(self.driver, ASGIMixin):
+            self.driver.setup_http_server(
+                HTTPServerSetup(
+                    path=URL("/webhook"),
+                    method="POST",
+                    name="monitor_webhook",
+                    handle_func=handle_webhook,
+                )
             )
+            logger.opt(colors=True).success(
+                f"🔍 监控服务已启动，监听地址: <cyan>{self.host}:{self.port}/webhook</cyan>")
--- nonebot-plugin-anipusher/starter.py
+++ nonebot-plugin-anipusher/starter.py
@@ -7,8 +7,8 @@ driver = get_driver()
 
 @driver.on_startup
 async def init_webhook():
-    from .core.monitor_core.health_check import NewHealthCheck
-    await NewHealthCheck.create_and_run()
+    # from .core.monitor_core.health_check import NewHealthCheck
+    # await NewHealthCheck.create_and_run()
     from .core.monitor_core.monitor import Monitor
     MONITOR = Monitor()
     await MONITOR.start_monitor()
